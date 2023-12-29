5bc015a2047a761f52df3863461f450205990c06
danicm93 <danicm.web@gmail.com>
2023-12-29 19:00:51 +0100

    Move route and theme to App component

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -1,16 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+import { Route, Routes , BrowserRouter } from 'react-router-dom'
 
 import { Home } from '@/pages'
 import { Layout } from '@/shared/components'
 
+import { theme } from '.'
+
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        {/* <Route path="contact" element={ <Contact /> } /> */}
-      </Routes>
-    </Layout>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            {/* <Route path="contact" element={ <Contact /> } /> */}
+          </Routes>
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
 
-import { ThemeProvider } from '@emotion/react'
-import { CssBaseline } from '@mui/material'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
-import { App, theme } from '@/core'
+import { App } from '@/core'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
