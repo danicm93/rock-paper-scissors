f534cf5fc099509cff94639d1437ef30c19ba807
danicm93 <danicm.web@gmail.com>
2024-01-01 11:52:02 +0100

    Refacto publicPrivateInterceptor

--- src/shared/interceptors/public-private.interceptor.tsx
+++ src/shared/interceptors/public-private.interceptor.tsx
@@ -3,14 +3,8 @@ import axios from 'axios'
 import { SnackbarUtilities, getValidationError } from '@/shared/utilities'
 
 export const PublicPrivateInterceptor = () => {
-  axios.interceptors.response.use(
-    (response) => {
-      return response
-    },
-    (error) => {
-      console.log('salimos por el error -->', error)
-      SnackbarUtilities.error(getValidationError(error.code))
-      return Promise.reject(error)
-    }
-  )
+  axios.interceptors.response.use(null, (error) => {
+    SnackbarUtilities.error(getValidationError(error.code))
+    return Promise.reject(error)
+  })
 }
--- src/shared/utilities/get-validation-error.utility.ts
+++ src/shared/utilities/get-validation-error.utility.ts
@@ -1,6 +1,6 @@
 import { TypeWithKey } from '@/shared/models'
 
-export const getValidationError = (errorCode: any) => {
+export const getValidationError = (errorCode: string) => {
   const codeMatcher: TypeWithKey<string> = {
     ERR_BAD_REQUEST: 'Error de comunicación con el servidor',
   }
