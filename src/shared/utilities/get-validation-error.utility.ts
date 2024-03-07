b35effc5f9a7ccf64d8ae7cc760869a6fdb4d533
danicm93 <danicm.web@gmail.com>
2024-03-07 12:24:47 +0100

    Fix error message

--- src/shared/utilities/get-validation-error.utility.ts
+++ src/shared/utilities/get-validation-error.utility.ts
@@ -3,6 +3,7 @@ import { TypeWithKey } from '@/shared/models'
 export const getValidationError = (errorCode: string) => {
   const codeMatcher: TypeWithKey<string> = {
     ERR_BAD_REQUEST: 'Error de comunicación con el servidor',
+    ERR_CANCELED: 'Petición cancelada',
   }
 
   return codeMatcher[errorCode]
